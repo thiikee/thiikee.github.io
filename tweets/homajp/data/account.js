1a8e3961b64ec78f0db34706a65afdba096afb59
thiikee <138864969+thiikee@users.noreply.github.com>
2025-01-16 18:37:28 +0900

    Add files via upload

---
+++ tweets/homajp/data/account.js
@@ -0,0 +1,11 @@
+window.YTD.account.part0 = [
+  {
+    "account" : {
+      "createdVia" : "web",
+      "username" : "homajp",
+      "accountId" : "209314251",
+      "createdAt" : "2010-10-28T23:30:03.000Z",
+      "accountDisplayName" : "ほま"
+    }
+  }
+]
